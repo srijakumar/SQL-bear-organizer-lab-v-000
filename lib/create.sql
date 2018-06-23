d76fb0090ca6b57dc57e882482acc2f0ac2aa7c3
Srija S <srijakumar7@gmail.com>
2018-06-23 15:55:27 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,1 @@
-CREATE TABLE bears (id INTEGER, name TEXT, age INTEGER, gender CHAR, color TEXT, temperament TEXT, alive INTEGER);
+CREATE TABLE bears (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, gender CHAR(1),color TEXT, temperament TEXT, alive INTEGER);
